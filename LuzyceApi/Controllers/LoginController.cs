71bd7bc85a923763c398ebd654420feee1b52a26
Daniel <daniel.dominik.nowicki@gmail.com>
2024-04-09 19:24:13 +0200

    move login to another controller

---
+++ LuzyceApi/Controllers/LoginController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuzyceApi.Data;
+using LuzyceApi.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LuzyceApi.Controllers
+{
+    [Route("api/login")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _config;
+
+        public LoginController(ApplicationDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        private string GenerateJSONWebToken(int userId)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SigningKey"] ?? ""));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] LoginDto dto)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Login == dto.Login && x.Password == dto.Password);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            var tokenString = GenerateJSONWebToken(user.Id);
+            return Ok(new { token = tokenString });
+        }
+    }
+}
--- LuzyceApi/Controllers/UserController.cs
+++ LuzyceApi/Controllers/UserController.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using LuzyceApi.Data;
 using LuzyceApi.Dtos.User;
 using LuzyceApi.Mappers;
-using LuzyceApi.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace LuzyceApi.Controllers
 {
@@ -20,28 +16,11 @@ namespace LuzyceApi.Controllers
     public class UserController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly IConfiguration _config;
 
-        public UserController(ApplicationDbContext context, IConfiguration config)
+        public UserController(ApplicationDbContext context)
         {
             _context = context;
-            _config = config;
         }
-
-        private string GenerateJSONWebToken(int userId)
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SigningKey"] ?? ""));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var token = new JwtSecurityToken(
-                issuer: _config["Jwt:Issuer"],
-                expires: DateTime.Now.AddHours(1),
-                signingCredentials: credentials
-            );
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
         private static string HashPassword(string password)
         {
             byte[] bytes = SHA256.HashData(System.Text.Encoding.UTF8.GetBytes(password));
@@ -50,18 +29,6 @@ namespace LuzyceApi.Controllers
             string hashedPassword = BitConverter.ToString(bytes).Replace("-", "").ToLower();
 
             return hashedPassword;
-        }
-
-        [HttpPost("login")]
-        public IActionResult Login([FromBody] LoginDto dto)
-        {
-            var user = _context.Users.FirstOrDefault(x => x.Login == dto.Login && x.Password == dto.Password);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            var tokenString = GenerateJSONWebToken(user.Id);
-            return Ok(new { token = tokenString });
         }
 
         [HttpGet]

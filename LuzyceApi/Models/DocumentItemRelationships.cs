ec561caf46a8f7f8e671cba62bc5de9046bb4e50
Daniel <daniel.dominik.nowicki@gmail.com>
2024-04-15 15:15:13 +0200

    add DocumentItemRelationships

---
+++ LuzyceApi/Models/DocumentItemRelationships.cs
@@ -0,0 +1,12 @@
+namespace LuzyceApi.Models;
+
+public class DocumentItemRelationships
+{
+    public int Id { get; set; }
+    public required Document ParentDocumentID { get; set; }
+    public required Document SubordinateDocumentID { get; set; }
+    public required DocumentPositions ParentPositionID { get; set; }
+    public required DocumentPositions SubordinatePositionID { get; set; }
+    public required int NetQuantityParent { get; set; }
+    public required int QuantityLossParent { get; set; }
+}

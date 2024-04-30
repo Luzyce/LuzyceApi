7cbb077ed5b0954b50a03282e8a684d13c2a30a7
Bartosz Nowicki <10196088+bdnowicki@users.noreply.github.com>
2024-04-30 21:34:24 +0200

    Update ApplicationDbContext.cs
    primary constructor usage
    + configuring mysql connection directly in DbContext

--- LuzyceApi.Db.AppDb/Data/ApplicationDbContext.cs
+++ LuzyceApi.Db.AppDb/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using LuzyceApi.Db.AppDb.Data.Models;
 
 namespace LuzyceApi.Db.AppDb.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions, IConfiguration config) : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
-    {
-    }
+    private readonly DbContextOptions<ApplicationDbContext> dbContextOptions = dbContextOptions;
+    private readonly IConfiguration config = config;
 
     public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(config.GetConnectionString("AppDbConnection"), ServerVersion.AutoDetect(connectionString));
+    }
 }

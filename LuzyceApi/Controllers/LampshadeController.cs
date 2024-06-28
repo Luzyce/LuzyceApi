908e4591054761ff87fc979ce430e10aae4ca935
Daniel <daniel.dominik.nowicki@gmail.com>
2024-06-28 14:50:54 +0200

    now lampshade controller use luzyce.core

--- LuzyceApi/Controllers/LampshadeController.cs
+++ LuzyceApi/Controllers/LampshadeController.cs
@@ -1,4 +1,5 @@
-﻿using LuzyceApi.Repositories;
+﻿using Luzyce.Core.Models.Lampshade;
+using LuzyceApi.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,28 +15,68 @@ public class LampshadeController(LampshadeRepository lampshadeRepository) : Cont
     [Authorize]
     public IActionResult GetLampshadeVariants()
     {
-        return Ok(lampshadeRepository.GetLampshadeVariants());
+        return Ok(new GetVariantsResponseDto
+        {
+            Variants = lampshadeRepository.GetLampshadeVariants().Select(x => new GetVariantResponseDto
+                {
+                    Id = x.Id,
+                    ShortName = x.ShortName,
+                    Name = x.Name
+                }).ToList()
+        });
     }
     
     [HttpGet("variants/{shortName}")]
     [Authorize]
     public IActionResult GetLampshadeVariant(string shortName)
     {
-        return Ok(lampshadeRepository.GetLampshadeVariant(shortName));
+        var variant = lampshadeRepository.GetLampshadeVariant(shortName);
+        
+        if (variant == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(new GetVariantResponseDto()
+        {
+            Id = variant.Id,
+            Name = variant.Name,
+            ShortName = variant.ShortName
+        });
     }
     
     [HttpGet("dekors")]
     [Authorize]
     public IActionResult GetLampshadeDekors()
     {
-        return Ok(lampshadeRepository.GetLampshadeDekors());
+        return Ok(new GetDekorsResponseDto()
+        {
+            Dekors = lampshadeRepository.GetLampshadeDekors().Select(x => new GetDekorResponseDto
+                {
+                    Id = x.Id,
+                    ShortName = x.ShortName,
+                    Name = x.Name
+                }).ToList()
+        });
     }
     
     [HttpGet("dekors/{shortName}")]
     [Authorize]
     public IActionResult GetLampshadeDekor(string shortName)
     {
-        return Ok(lampshadeRepository.GetLampshadeDekor(shortName));
+        var dekor = lampshadeRepository.GetLampshadeDekor(shortName);
+        
+        if (dekor == null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(new GetDekorResponseDto()
+        {
+            Id = dekor.Id,
+            Name = dekor.Name,
+            ShortName = dekor.ShortName
+        });
     }
 }
 

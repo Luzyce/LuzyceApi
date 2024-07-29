440574f8f66e0add98d977ec1a4e65b47108e629
Daniel <daniel.dominik.nowicki@gmail.com>
2024-07-29 12:52:56 +0200

    add GetProductionPlans

---
+++ LuzyceApi.Repositories/ProductionPlanRepository.cs
@@ -0,0 +1,36 @@
+﻿using Luzyce.Core.Models.Document;
+using Luzyce.Core.Models.ProductionPlan;
+using Luzyce.Core.Models.User;
+using LuzyceApi.Db.AppDb.Data;
+
+namespace LuzyceApi.Repositories;
+
+public class ProductionPlanRepository(ApplicationDbContext applicationDbContext)
+{
+    public GetProductionPlans GetProductionPlans(GetMonthProductionPlanRequest request)
+    {
+        return new GetProductionPlans
+        {
+            ProductionPlans = applicationDbContext.ProductionPlans
+                .Where(x => x.Date.Month == request.ProductionPlanDate.Month && x.Date.Year == request.ProductionPlanDate.Year)
+                .Select(x => new GetProductionPlan
+                {
+                    Id = x.Id,
+                    Date = x.Date,
+                    Change = x.Change,
+                    Team = x.Team,
+                    Metallurgist = x.Metallurgist == null ? null : new GetUserResponseDto
+                    {
+                        Id = x.Metallurgist.Id,
+                        Name = x.Metallurgist.Name
+                    },
+                    Status = x.Status == null ? null : new GetStatusResponseDto
+                    {
+                        Id = x.Status.Id,
+                        Name = x.Status.Name
+                    }
+                })
+                .ToList()
+        };
+    }
+}
---
+++ LuzyceApi/Controllers/ProductionPlanController.cs
@@ -0,0 +1,18 @@
+﻿using Luzyce.Core.Models.ProductionPlan;
+using LuzyceApi.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuzyceApi.Controllers;
+
+[ApiController]
+[Route("api/productionPlan")]
+public class ProductionPlanController(ProductionPlanRepository productionPlanRepository) : Controller
+{
+    [HttpPost]
+    [Authorize]
+    public IActionResult GetProductionPlans(GetMonthProductionPlanRequest request)
+    {
+        return Ok(productionPlanRepository.GetProductionPlans(request));
+    }
+}
